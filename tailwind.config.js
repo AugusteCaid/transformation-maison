36c15b7fb777784bde6d1dc214bef33dbecb43c6
wkava57 <wkava57@hotmail.com>
2023-03-15 13:54:10 +0100

    premier carrousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,31 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'glisse': 'glisse 15s infinite ease-in-out'
+      },
+      keyframes: {
+        glisse: {
+          // largeur de reference de base 650px
+          '100%': {transform: 'translatex(-3900px)'},
+        }
+      }
+
+
+      
+    },
+    screens: {
+      // @media (max-width: 2500px) {...}
+      '3xl': {'max': '2560px'},
+      // @media (max-width: 1440px) {...}
+      '2xl': {'max': '1440px'},
+      'xl': {'max': '1024px'},
+      'lg': {'max': '768px'},
+      'md': {'max': '425px'},
+      'sm': {'max': '320px'},
+
+    }
   },
   plugins: [],
 }
