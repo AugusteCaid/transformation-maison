81f9b2c9a45b108331633ea49364261428c85630
wkava57 <wkava57@hotmail.com>
2023-03-27 16:33:39 +0200

    update page maison

--- pages/maison.tsx
+++ pages/maison.tsx
@@ -15,112 +15,92 @@ import image6 from '../public/images/renovf.png'
 
 const Home: NextPage = () => {
     return (
-      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <div className="flex min-h-screen flex-col items-center justify-center">
         <Head>
           <title>Create Next App</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
         <main>
-            <div className="title w-full flex justify-center items-center">
-              <h1 className='uppercase font-bold text-6xl 
-              2xl:h-
-              xl:text-2xl'>galerie photos</h1>
+        <div className="title py-12 text-center bg-blue-200">
+          <h1 className=' w-[100%] text-5xl font-bold uppercase'>Travaux de renovation</h1>
+
+        </div>
+
+        <div className="home w-full h-screen flex justify-center items-center ">
+          <div className="avant  w-[100%] ">
+            <div className="before w-[75%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
+              2xl:
+              xl:w-1/5">
+              <Image className='rounded-2xl ' 
+              src={avant}
+              alt="une pièce avant la renovation"></Image>
             </div>
-            
-            <div className='home w-screen h-screen flex justify-around items-center m-0 p-0
-              xl:block'>
-              
+            <br />
+            <h3 className='w-2/4 text-center text-center bg-blue-500 text-white m-auto rounded-2xl
+              xl:w-1/5'>
+              Avant
+            </h3>                
+            <br />
+          </div>
 
-              <div className="avant">
-                <div className="before w-[70%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
-                  xl:w-1/5">
-                  <Image className='rounded-2xl ' 
-                  src={avant} alt="une pièce avant la renovation"></Image>
-                </div>
-                <br />
-                <h3 className='w-3/4 text-center text-center bg-blue-500 text-white m-auto
-                  xl:w-1/5'>
-                  Avant
-                </h3>                
-                <br />
+          <div className="twobloc">
+            <div className=" overflow-hidden m-auto w-[100%*6] w-[45%] h-[40%]  rounded-2xl shadow-[10px_10px_101px_0_rgb(0,0,0)]">
+
+              <div className="slides  flex shadow-[10px_10px_101px_0_rgb(0,0,0)] animate-animate">
+
+                <Image className='w-[100%] slide float-left text-center'
+                  src={image1} alt="première diapo de la carrousel"></Image>
+                  
+                <Image className='w-[100%] slide float-left'
+                  src={image2} alt="deuxième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image3} alt="troisième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image4} alt="quatrième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image5} alt="cinquième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image6} alt="sixième diapo de la carrousel"></Image>
+                <Image className='w-[100%] slide float-left'
+                  src={image1} alt="retour à la première diapo"></Image>
+
               </div>
+            </div>
+            <br /><br />           
               
-              <div>
-                <div className="slider overflow-hidden m-auto w-[650px*7] w-[80%]  h-[40%] w-[650px*7] rounded-2xl shadow-  [10px_10px_101px_0_rgb(0,0,0)]"
-                  // 3xl:w-[650px*7] 3xl:w-[650px] 3xl:h-[35%] 
-                  // 2xl:w- 2xl:w-[71%] 2xl:h-[50%]
-                  // xl:w-[650px*7] xl:w-[40%] m-auto
-                  // lg:w-[650px*7] lg:w-[54.7%] "
-                >
-                  <div className="slides flex shadow-[10px_10px_101px_0_rgb(0,0,0)] animate-animate">
-
-                    <Image className='w-full slide float-left text-center' 
-                    src={image1} alt="première diapo de la carrousel"></Image>                  
-                    <Image className='w-full slide float-left'
-                    src={image2} alt="deuxième diapo de la carrousel"></Image>
-                    <Image className='w-full slide float-left' 
-                    src={image3} alt="troisième diapo de la carrousel"></Image>
-                    <Image className='w-full slide float-left' 
-                    src={image4} alt="quatrième diapo de la carrousel"></Image>
-                    <Image className='w-full slide float-left' 
-                    src={image5} alt="cinquième diapo de la carrousel"></Image>
-                    <Image className='w-full slide float-left' 
-                    src={image6} alt="sixième diapo de la carrousel"></Image>
-                    <Image className='w-full slide float-left' 
-                    src={image1} alt="retour à la première diapo"></Image>
-                  </div>                
-                </div>
-                <br />
-
-                <div className="slider overflow-hidden w-[80%]  h-[40%] w-[650px*7] rounded-2xl shadow-[10px_10px_101px_0_rgb(0,0,0)] m-auto"
-                  // 3xl:w- 3xl:w-[72%] 3xl:h-[35%] 
-                  // 2xl:w-[650px*7] 2xl:w-[60%] 2xl:h-[50%]
-                  // xl:w-[650px*7] xl:w-[40.5%] xl:h-[40%] m-auto
-                  // lg:w-[650px*7] lg:w-[55%] "
-                >
-                  <div className="slides w-[600%] text-center flex shadow-[10px_10px_101px_0_rgb(0,0,0)] animate-glisse
-                    // 3xl:w-[700%] 
-                    // 2xl:w- 2xl:w-[712%] 
-                    // xl:w-[650px*7]  xl:w-[696%]
-                    ">
-
-                      <p className='w-[100%] slide ml-24 3xl:m-0 2xl:'>travaux 1er pièce
-                      </p>
-                      <p className='w-[100%] slide float-left 
-                      
-                      '>travaux 2ème pièce</p>
-                      <p className='w-[100%] slide float-left
-                      
-                      '>travaux 4ème pièce</p>
-                      <p className='w-[100%] slide float-left
-                      
-                      '>Pièce de stockage</p>
-                      <p className='w-[100%] slide float-left
-                      
-                      '>Ponçage plafond</p>
-                      <p className='w-[100%] slide float-left
-                      
-                      '>Préparation peinture</p>
-                      <p className='w-[100%] slide float-left
-                      
-                      '>travaux 1er pièce</p>
-
+                <div className="carrousel text-center font-[30px] leading-[45px] ">
+                  {/* <div className="pretxt h-[5px] w-1/2 text-blue-500 font-bold shadow-black uppercase ">je vous présente :</div> */}
+                  <div className="changetxt text-center h-[45px] overflow-hidden ">
+                    <div className="content font-bold font- uppercase animate-carrousel">
+                      <div className="element">Peinture chambre 1</div>
+                      <div className="element">Préparation de bande de protection</div>
+                      <div className="element">Pose de placo salon</div>
+                      <div className="element">Pièce de stockage de matériels</div>
+                      <div className="element">Ponçage du plafond</div>
+                      <div className="element">Chambre 2 en travaux</div>
+                      <div className="element">Peinture chambre 1</div>
+                    </div>
                   </div>
                 </div>
-              </div>
+            
+          </div>
 
-              <div className="apres xl:">
-                <div className="after m-auto w-[70%] h-[60%] shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
-                  xl:w-1/5">
-                  <Image className='rounded-2xl' src={apres} alt="diapo de la pièce terminé de renover"></Image>
-                </div>
-                <br />
-                <h3 className='w-3/4 text-center text-center bg-blue-500 text-white m-auto
-                  xl:w-1/5'>Après</h3>
-              </div>    
-
+          <div className="apres w-[100%] ">
+            <div className="after w-[75%] h-[60%] m-auto shadow-[10px_10px_101px_0_rgb(0,0,0)] rounded-2xl
+              xl:w-1/5">
+              <Image className='rounded-2xl ' 
+              src={apres} alt="une pièce avant la renovation"></Image>
             </div>
+            <br />
+            <h3 className='w-2/4 text-center text-center  bg-blue-500 text-white m-auto rounded-2xl
+              xl:w-1/5'>
+              Apres
+            </h3>                
+            <br />
+          </div>
+
+        </div> 
         </main>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,18 +8,26 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        'glisse': 'glisse 15s infinite ease-in-out',
-            
+        'animate': 'animate 15s infinite ease-in-out',
+        'carrousel': 'carrousel 15s infinite ease-in-out'
       },
 
         
 
       keyframes: {
-        glisse: {
-          // largeur de reference de base 650px
-          '100%': {transform: 'translatex(-3900px)'}
+        animate: {
+          // largeur de reference de base 650px          
+            '100%': {transform: 'translatex(-600%)'}
         },
-        
+        carrousel: {
+          '0%, 10%': { transform: 'translatey(0)' },
+          '15%, 25%': { transform: 'translatey(-45px)' },
+          '30%, 40%': { transform: 'translatey(-90px)' },
+          '45%, 55%': { transform: 'translatey(-135px)' },
+          '60%, 70%': { transform: 'translatey(-180px)' },
+          '75%, 85%': { transform: 'translatey(-225px)' },
+          '90%, 100%': { transform: 'translateY(-270px)'}
+        },
       },
 
       
